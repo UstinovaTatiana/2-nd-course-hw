f978516fa00f9b77330e69b6a426f04845016924
UstinovaTatiana <ustinta@gmail.com>
2025-05-03 20:18:25 +0300

    Реализованна игра 2

--- script.js
+++ script.js
@@ -32,3 +32,68 @@ function guessNumber() {
 }
 let randomInt = Math.floor(Math.random() * 100) + 1;
 
+
+
+
+function getUserAnswer() {
+    let input = prompt(`Попробуйте решить ${num1} ${randomOperation} ${num2}`);
+    if (input === null) {
+        return null;
+    }
+    let userAnswer = parseFloat(input);
+    if (isNaN(userAnswer) || !Number.isFinite(userAnswer)) {
+        alert('Пожалуйста, введите ответ.');
+        return getUserAnswer();
+    }
+    return userAnswer;
+}
+
+const operation = ['+', '-', '*', '/'];
+const randomOperation = operation[Math.floor(Math.random() * operation.length)];
+/*console.log(randomOperation);*/
+let num1 = Math.floor(Math.random() * 20);
+let num2 = Math.floor(Math.random() * 20);
+/*console.log(num1);
+console.log(num2);*/
+
+function calculate(operation, num1, num2) {
+    switch (operation) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+
+        default:
+            return 'invalid operation';
+    }
+
+
+}
+/*console.log(calculate(randomOperation, num1, num2));*/
+
+function guessOperation() {
+    while (true) {
+        let userAnswer = getUserAnswer();
+
+        if (userAnswer === null) {
+            break;
+        }
+        if (userAnswer === result) {
+            alert(`Верно`);
+            return;
+        }
+        else {
+            alert('Не верно');
+        }
+
+    }
+}
+let result = calculate(randomOperation, num1, num2);
+
+/*console.log(result);*/
+
+
